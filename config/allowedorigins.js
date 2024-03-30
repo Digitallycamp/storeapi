2383bd9cc841d3647d57290696fef8069b7f81c8
Digitallycamp <crytechcrytech@gmail.com>
2024-03-30 05:11:34 -0700

    added  more core options

--- config/allowedorigins.js
+++ config/allowedorigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5501',
+    'http://localhost:5173',
+    'http://127.0.0.1:5501',
     'https://store-api-ycui.onrender.com/'
 ]
 
