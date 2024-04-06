a2b1d946bcbbabb2fb70df0a0a5f25ff37a9b557
Digitallycamp <crytechcrytech@gmail.com>
2024-04-06 08:20:36 -0700

    allowed http://127.0.0.1:5500 for CORS

--- config/allowedorigins.js
+++ config/allowedorigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5501',
     'http://127.0.0.1:5502',
+    'http://127.0.0.1:5500',
     'https://store-api-ycui.onrender.com/'
 ]
 
