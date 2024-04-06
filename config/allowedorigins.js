f2538a2d803affc5a55b6f90e335e312a2d14408
Digitallycamp <crytechcrytech@gmail.com>
2024-04-06 13:51:53 -0700

    updated readt cors

--- config/allowedorigins.js
+++ config/allowedorigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5501',
     'http://localhost:5173',
+    'http://127.0.0.1:5173',
     'http://127.0.0.1:5501',
     'http://127.0.0.1:5502',
     'http://127.0.0.1:5500',
